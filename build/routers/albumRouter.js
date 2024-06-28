d441490d2c6cf02c9e0a70852027a39e5d42a81e
enmarcm <theenmanuel123@gmail.com>
2024-06-28 16:31:09 -0400

    changes

--- build/routers/albumRouter.js
+++ build/routers/albumRouter.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const albumController_1 = __importDefault(require("../controllers/albumController"));
 const albumRouter = (0, express_1.Router)();
+albumRouter.get("/getId/:id", albumController_1.default.getAlbumById);
 albumRouter.get("/top", albumController_1.default.getTopAlbum);
 albumRouter.get("/artist/:id", albumController_1.default.getAlbumByIdArtist);
 exports.default = albumRouter;
--- src/routers/albumRouter.ts
+++ src/routers/albumRouter.ts
@@ -3,6 +3,7 @@ import AlbumController from "../controllers/albumController";
 
 const albumRouter = Router();
 
+albumRouter.get("/getId/:id", AlbumController.getAlbumById);
 albumRouter.get("/top", AlbumController.getTopAlbum);
 albumRouter.get("/artist/:id", AlbumController.getAlbumByIdArtist);
 
